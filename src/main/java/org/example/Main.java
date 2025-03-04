540043cd4dd21932218804410710f2e201f24ef4
Seok1225 <koreait6642@gmail.com>
2025-03-04 10:44:44 +0900

    자바3강

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,17 +2,23 @@ package org.example; // 이 부분은 아직 모르셔도 됩니다.
 
 public class Main {
     public static void main(String[] args) {
-        // System.out 여기서 . 은 of 를 의미 합니다.
-        // System.out.println(100); // 여기서 println(100); 와 같은 형태는 타 동사를 의미합니다.
-        // System.out.println(100); // 여기서 System.out 은 주어를 의미 합니다.
-        // System.out.println(100); // 여기서 100은 목적어 또는 보어 입니다.
-        // System.out.println(100); // 개발자가 "System.out" 라는 녀석에게 100 을 println 하라는 의미
+        System.out.print("안녕");
+        System.out.print("안녕");
+        System.out.print("안녕");
 
-        // 프로그램의 시작점
-        System.out.println(1); // 실행순서 1 : 숫자는 " 로 감싸지 않아도 됩니다.
-        System.out.println("2");
-        System.out.println("3");
-        System.out.println("사");
-        // 프로그램의 끝점
+        // 출력
+        // 안녕안녕안녕
+
+        System.out.println(); // 이 코드는 System.out.print("\n"); 과 같다.
+
+        // 출력
+        // \n
+        // 추가설명 : 실제로 출력창에 \n 는 보이지 않고 줄바꿈으로 작동한다.
+
+        System.out.println("안녕\n안녕안녕");
+
+        // 출력
+        // 안녕
+        // 안녕안녕
     }
 }
