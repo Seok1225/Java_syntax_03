d4cb59263aa47028c6c6e05b2fe69f18142cef3c
Seok1225 <koreait6642@gmail.com>
2025-03-04 11:32:29 +0900

    자바 테스트

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,11 +1,33 @@
-package org.example; // 이 부분은 아직 모르셔도 됩니다.
+package org.example;
 
-public class Main {
+// 문제 : a와 b가 가지고 있는 값을 서로 뒤바꿔주세요. // 조건 : 숫자와 사칙연산 사용 금지
+
+class Main {
     public static void main(String[] args) {
-        int x; // 변수
-        x = 5; // 변수
-        x = x + 10; // 좌측 x는 변수, 우측 x는 변수가 아닌 값(5)으로 취급
+        // `int a` => a라는 이름의 변수(즉 공간)을 만든다.
+        // `int` => a라는 공간에는 정수만 담을 수 있다.
+        // `;` => 문장이 끝남을 알린다.
+        int a;
+        a = 5;
 
-        System.out.println(x + 20); // 여기서의 x는 x가 아닌 15로 취급
+        System.out.println(a);
+        System.out.println(a + 10);
+
+        int b = 10;
+
+        // `+` => 문장과 문자을 합친다.
+        System.out.println("a : " + a);
+        System.out.println("b : " + b);
+
+        // 여기서 부터
+        int c = a;
+        a = b;
+        b = c;
+        // 여기까지 수정 가능
+
+        System.out.println("a : " + a);
+        // 출력 : a : 10
+        System.out.println("b : " + b);
+        // 출력 :  b : 5
     }
 }
