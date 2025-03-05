e43816d7fa6cf912ed1c421210d53fd003d516ab
Seok1225 <koreait6642@gmail.com>
2025-03-05 11:57:43 +0900

    구구단 8단 1부터 1000까지

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,59 +1,18 @@
 package org.example;
-import javax.swing.*;
 
 class Main {
     public static void main(String[] args) {
-        System.out.println("첫번째");
-        int a = 8;
 
-        System.out.println("8 * 1 :" + a);
-        System.out.println("8 * 2 :" + a * 2);
-        System.out.println("8 * 3 :" + a * 3);
-        System.out.println("8 * 4 :" + a * 4);
-        System.out.println("8 * 5 :" + a * 5);
-        System.out.println("8 * 6 :" + a * 6);
-        System.out.println("8 * 7 :" + a * 7);
-        System.out.println("8 * 8 :" + a * 8);
-        System.out.println("8 * 9 :" + a * 9);
+        gugudan(8,1);
 
-        System.out.println("두번째");
-        int dan = 8;
 
-        System.out.println("8 * 1 = " + dan);
-        System.out.println("8 * 2 = " + dan * 2);
-        System.out.println("8 * 3 = " + dan * 3);
-        System.out.println("8 * 4 = " + dan * 4);
-        System.out.println("8 * 5 = " + dan * 5);
-        System.out.println("8 * 6 = " + dan * 6);
-        System.out.println("8 * 7 = " + dan * 7);
-        System.out.println("8 * 8 = " + dan * 8);
-        System.out.println("8 * 9 = " + dan * 9);
-
-        System.out.println("세번째");
-        int dan = 8;
-
-        System.out.println(dan + " * 1 = 8");
-        System.out.println(dan + " * 2 = 16");
-        System.out.println(dan + " * 3 = 24");
-        System.out.println(dan + " * 4 = 32");
-        System.out.println(dan + " * 5 = 40");
-        System.out.println(dan + " * 6 = 48");
-        System.out.println(dan + " * 7 = 56");
-        System.out.println(dan + " * 8 = 64");
-        System.out.println(dan + " * 9 = 72");
-
-        System.out.println("네번째");
-        int dan = 8;
-
-        System.out.println(dan + " * 1 = " + dan);
-        System.out.println(dan + " * 2 = " + dan * 2);
-        System.out.println(dan + " * 3 = " + dan * 3);
-        System.out.println(dan + " * 4 = " + dan * 4);
-        System.out.println(dan + " * 5 = " + dan * 5);
-        System.out.println(dan + " * 6 = " + dan * 6);
-        System.out.println(dan + " * 7 = " + dan * 7);
-        System.out.println(dan + " * 8 = " + dan * 8);
-        System.out.println(dan + " * 9 = " + dan * 9);
-
+    }
+    public  static  void gugudan(int dan , int i)
+    {
+        if(i >= 1000)return;
+        {
+            System.out.println(dan + "x" +i +"=" + dan * i);
+            gugudan(dan, i + 1);
+        }
     }
 }
