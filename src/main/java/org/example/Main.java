a86ac530e3bd737db2c4cfdc5b731551c3e35ca2
Seok1225 <koreait6642@gmail.com>
2025-03-05 12:50:41 +0900

    반복문 문제3

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,13 +1,23 @@
 package org.example;
-// 문제 : -100부터 25까지 출력해주세요.
+// 문제 : 아래와 같이 출력해주세요.
+// 문제 : 단을 `* 1000`부터 `* 1`까지 출력해주세요.
+
 class Main {
     public static void main(String[] args) {
+        int dan = 8;
 
-        int i = -100;
+        int i = 1000;
 
-        while(i <= 25) {
-            System.out.println(i);
-            i++;
+        while(i >= 1)
+        {
+            System.out.println(dan + " * " + i + " = " + dan * i);
+            i--;
         }
+        // 출력
+    /*
+    8 * 1000 = 8000
+    ...
+    8 * -500 = -4000
+    */
     }
 }
