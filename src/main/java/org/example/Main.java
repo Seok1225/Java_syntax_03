143adea3bbfbad70138fc1e6a19d73114822d354
Seok1225 <koreait6642@gmail.com>
2025-03-05 13:19:02 +0900

    반복문 문제6

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,18 +1,18 @@
 package org.example;
 // 문제 : 아래와 같이 출력해주세요.
-// 문제 : 1부터 5까지의 합을 출력해주세요.
+// 문제 : -100부터 25까지의 합을 출력해주세요.
 
 class Main {
     public static void main(String[] args) {
 
-        int i = 1;
+        int i = -100;
         int sum = 0;
 
-        while(i <= 5)
+        while(i < 25)
         {
             sum += i;
             i++;
         }
-        System.out.println("1부터 5까지의 합은 = " + sum + " 입니다 ");
+        System.out.println("-100부터 25까지의 합은 = " + sum + " 입니다 ");
     }
 }
