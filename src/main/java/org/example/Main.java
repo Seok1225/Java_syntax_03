028f39e5f429a642b4b49548875393ff3b525ef8
Seok1225 <koreait6642@gmail.com>
2025-03-05 14:30:55 +0900

    반복문 문제7

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,18 +1,21 @@
 package org.example;
-// 문제 : 아래와 같이 출력해주세요.
-// 문제 : -100부터 25까지의 합을 출력해주세요.
+// 문제 : 1부터 3까지 출력하는 작업을 10번 해주세요.
+// 조건 : 2중 while문 사용
 
 class Main {
     public static void main(String[] args) {
 
-        int i = -100;
-        int sum = 0;
+        int i , j;
 
-        while(i <= 25)
-        {
-            sum += i;
+        i = 1;
+        while(i <= 10) {
+            j = 1;
+            while(j < 4) {
+                System.out.println(j);
+                j++;
+            }
+            System.out.println("---" + i+"번째");
             i++;
         }
-        System.out.println("-100부터 25까지의 합은 = " + sum + " 입니다 ");
     }
 }
