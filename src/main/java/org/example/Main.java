30278f5f09226a757f838d26b779048e65cc8e45
Seok1225 <koreait6642@gmail.com>
2025-03-10 09:53:23 +0900

    배열

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,21 +1,34 @@
 package org.example;
-// 문제 : 1부터 3까지 출력하는 작업을 10번 해주세요.
-// 조건 : 2중 while문 사용
-
+import java.util.Scanner;
+// 1. arr1 변수 [true, false , true]
+// 2. arr2 변수 [3.14, 7,77, 11,,11]
+// 3. arr3 변수 [1~10]
 class Main {
     public static void main(String[] args) {
+     // 1번
+//        boolean[] arr1 = new boolean[3];
+//        arr1[0] = true;
+//        arr1[1] = false;
+//        arr1[2] = true;
+//
+//        System.out.println(arr1[0]);
+//        System.out.println(arr1[1]);
+//        System.out.println(arr1[2]);
 
-        int i , j;
+//        double[] arr1 = new double[3];
+//        arr1[0] = 3.14;
+//        arr1[1] = 7.77;
+//        arr1[2] = 11.11;
+//
+//        System.out.println(arr1[0]);
+//        System.out.println(arr1[1]);
+//        System.out.println(arr1[2]);
 
-        i = 1;
-        while(i <= 10) {
-            j = 1;
-            while(j < 4) {
-                System.out.println(j);
-                j++;
-            }
-            System.out.println("---" + i+"번째");
-            i++;
-        }
+//        int[] arr1 = new int[10];
+//
+//        for(int i = 0; i < arr1.length; i++){
+//            arr1[i] = i + 1;
+//            System.out.println(arr1[i]);
+//        }
     }
 }
