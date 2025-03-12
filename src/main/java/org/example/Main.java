b7bd4b79599d738806850521548ee3943c5c1baf
Seok1225 <koreait6642@gmail.com>
2025-03-12 10:02:14 +0900

    소수 판별기

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,34 +1,32 @@
 package org.example;
-
-import java.awt.*;
-import java.util.Arrays;
-
+// 1부터 n까지 소수의 (갯수)를 출력하는 함수 만들기
 class Main {
     public static void main(String[] args) {
-        String msg = greet("Jin");
-        System.out.println(msg);
-
-        Point point = createPoint(5, 10);
-        System.out.println("Point create at (" + point.x + ", " + point.y + ")");
-
-        int[] evens = firstNEvenNumbers(5);//
-        System.out.println("first 5 even numbers : " + Arrays.toString(evens));
+        int n = 100; // 예시로 10까지의 소수를 구합니다.
+        int primeCount = Primes.countPrimes(n);
+        System.out.println("1부터 " + n + "까지의 소수의 갯수: " + primeCount);
     }
+}
 
-    public static String greet(String name) {
-        return "Hello, " + name + "!";
-    }
-
-    public static Point createPoint(int x, int y) {
-        return new Point(x, y);
-    }
-
-    public static int[] firstNEvenNumbers(int n) {
-        int[] evenNumbers = new int[n];
-
-        for (int i = 0, num = 2; i < n; i++, num += 2) {
-            evenNumbers[i] = num;
+class Primes { // 1부터 n까지의 소수의 갯수를 세는 메소드
+    static int countPrimes(int n) {
+        int count = 0;
+        for (int i = 2; i <= n; i++) {
+            if (isPrimeNumber(i)) {
+                System.out.println(i + " ");
+                count++;
+            }
         }
-        return evenNumbers;
+        return count;
+    }
+
+    static boolean isPrimeNumber(int num) { // 주어진 숫자가 소수인지 판별하는 메소드
+        if (num < 2) return false; // 2보다 작은 수는 소수가 아님
+        for (int i = 2; i <= Math.sqrt(num); i++) { // 제곱근까지만 확인
+            if (num % i == 0) {
+                return false; // 약수가 있으면 소수가 아님
+            }
+        }
+        return true; // 소수임
     }
 }
