c686c24ec713ee8c68bbc9b9074b2cd1ef615117
Seok1225 <koreait6642@gmail.com>
2025-03-17 13:35:54 +0900

    a*b

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,9 +5,9 @@ import java.util.Scanner; // 스캐너 클래스 호출
 class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in); // 스캐너 객체 생성
-        int a,b; // 정수만 담을수있는 변수 ab생성
-        a = sc.nextInt(); //a변수에 정수형 입력 및 리턴
-        b = sc.nextInt(); //b변수에 정수형 입력 및 리턴 
-        System.out.println(a * b); //값을 곱함
+        double a,b; // 실수만 담을수있는 변수 ab생성
+        a = sc.nextDouble(); //a변수에 실수형 입력 및 리턴
+        b = sc.nextDouble(); //b변수에 실수형 입력 및 리턴
+        System.out.println(a / b); //값을 나눔
     }
 }
