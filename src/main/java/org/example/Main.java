60a7f2c28f82f8c0112c77c72e482256324a3988
Seok1225 <koreait6642@gmail.com>
2025-03-17 13:38:27 +0900

    a/b

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,6 +8,6 @@ class Main {
         double a,b; // 실수만 담을수있는 변수 ab생성
         a = sc.nextDouble(); //a변수에 실수형 입력 및 리턴
         b = sc.nextDouble(); //b변수에 실수형 입력 및 리턴
-        System.out.println(a / b); //값을 나눔
+        System.out.println(a / b); //값을 나누기
     }
 }
