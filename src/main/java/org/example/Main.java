bd0c847ede21c25d567367c13d4ad7467121d021
Seok1225 <koreait6642@gmail.com>
2025-03-17 15:50:33 +0900

    알람시계

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,17 +5,19 @@ import java.util.Scanner;
 class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int x = sc.nextInt();
-        int y = sc.nextInt();
 
-        if (x != 0 && y != 0) {
-            if (x > 0 && y > 0)
-                System.out.print("1");
-            else if (x < 0 && y > 0)
-                System.out.print("2");
-            else if (x < 0)
-                System.out.print("3");
-            else System.out.print("4");
+        int h = sc.nextInt();
+        int m = sc.nextInt();
+        m -=45;
+        if (m < 0) {
+            h--;
+            m+=60;
         }
+        if (h >= 24) {
+            h = 0;
+        }else if(h<0){
+            h = 23;
+        }
+        System.out.println(h  + " " + m);
     }
 }
