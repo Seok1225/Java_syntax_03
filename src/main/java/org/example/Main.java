472175411fc837079617dc641b144e939c8f068e
Seok1225 <koreait6642@gmail.com>
2025-03-17 13:29:24 +0900

    a-b

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,7 +7,7 @@ class Main {
         Scanner sc = new Scanner(System.in); // 스캐너 객체 생성
         int a,b; // 정수만 담을수있는 변수 ab생성
         a = sc.nextInt(); //a변수에 정수형 입력 및 리턴
-        b = sc.nextInt(); //b변수에 정수형 입력 및 리턴
-        System.out.println(a + b); //값을 더함
+        b = sc.nextInt(); //b변수에 정수형 입력 및 리턴 
+        System.out.println(a - b); //값을 뺌
     }
 }
