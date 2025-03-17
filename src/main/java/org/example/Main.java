4dd53a7681526195b676d3710c2eb32ed5e94825
Seok1225 <koreait6642@gmail.com>
2025-03-17 16:10:37 +0900

    오븐 시계

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,18 +6,19 @@ class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int h = sc.nextInt();
-        int m = sc.nextInt();
-        m -=45;
-        if (m < 0) {
-            h--;
-            m+=60;
-        }
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+
+        int min = (a * 60) + b;
+        min = min + c;
+        int h = min / 60;
+        int m = min % 60;
+
         if (h >= 24) {
-            h = 0;
-        }else if(h<0){
-            h = 23;
+            System.out.println(h % 24 + "" + m);
+        } else {
+            System.out.println(h + " " + m);
         }
-        System.out.println(h  + " " + m);
     }
 }
