c4af00f4af010d5e4f7f39c3e0347eb317c7b0cc
Seok1225 <koreait6642@gmail.com>
2025-03-17 15:09:32 +0900

    개

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,10 +5,11 @@ import java.util.Scanner;
 class Main{
     public static void main(String[] args) {
         System.out.println("""
-\\    /\\
- )  ( ')
-(  /  )
- \\(__)|
+|\\_/|
+|q p|   /}
+( 0 )""\"\\
+|"^"`    |
+||_/=\\\\__|
      """);
     }
 }
