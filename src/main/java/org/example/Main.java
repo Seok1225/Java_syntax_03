4473fc7809d5e9d9e5541eab44be469707d8aa93
Seok1225 <koreait6642@gmail.com>
2025-03-17 14:10:06 +0900

    1998년생인 내가 태국에서는 2541년생?!

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,11 +1,12 @@
 package org.example;
 
-import java.util.Scanner; // 스캐너 클래스 호출
+import java.util.Scanner;
 
 class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String s = sc.nextLine();
-        System.out.printf(s + "??!\n");
+        int a = sc.nextInt();
+        int s = a - 543;
+        System.out.println(s);
     }
 }
