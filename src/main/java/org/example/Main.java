8c6d80d3b45fc4a90e8cb80cc6e734d99374b167
Seok1225 <koreait6642@gmail.com>
2025-03-17 13:26:57 +0900

    a+b

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,13 @@
 package org.example;
 
+import java.util.Scanner; // 스캐너 클래스 호출
+
 class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!"); // 화면에 문자열(Hello World) 를 출력하겟다.
+        Scanner sc = new Scanner(System.in); // 스캐너 객체 생성
+        int a,b; // 정수만 담을수있는 변수 ab생성
+        a = sc.nextInt(); //a변수에 정수형 입력 및 리턴
+        b = sc.nextInt(); //b변수에 정수형 입력 및 리턴
+        System.out.println(a + b); //값을 더함
     }
 }
