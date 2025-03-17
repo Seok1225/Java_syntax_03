f04e48780e0243dd3bf6ae1ddf7d53a4078c78bd
Seok1225 <koreait6642@gmail.com>
2025-03-17 15:09:13 +0900

    고양이

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,14 +2,13 @@ package org.example;
 
 import java.util.Scanner;
 
-public class Main {
+class Main{
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        long a = sc.nextLong();
-        long b = sc.nextLong();
-        long c = sc.nextLong();
-
-        System.out.println(a + b + c);
-
+        System.out.println("""
+\\    /\\
+ )  ( ')
+(  /  )
+ \\(__)|
+     """);
     }
 }
